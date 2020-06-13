da46d0f7a5ec8b8a8c32fa4a90ff32fb6d527bef
Alejandro Delgado <adelgado@iag.usp.br>
2020-06-12 20:05:49 -0500

    PY download aq and met data from CETESB

---
+++ download_CETESB.py
@@ -0,0 +1,28 @@
+# Downloading meteorological and air quality parameters from CETESB stations
+import numpy as np
+import pandas as pd
+
+cetesb_stations = pd.read_csv(
+    '../3_Validation/cetesb_station_2017_codes_qualr.csv', encoding = "ISO-8859-1")
+cetesb_stations[1:]
+
+import qualar_py as qr
+obs_dates = pd.DataFrame({
+    'date': pd.date_range('2016-08-01', 
+    periods=31*24, 
+    freq='H')
+})
+obs_dates['date_qualar']=obs_dates['date'].dt.strftime('%d/%m/%Y')
+
+cetesb_login =  input('login:') 
+cetesb_password = input('password:')
+start_date = obs_dates.date_qualar.values[0]
+end_date = obs_dates.date_qualar.values[-1]
+
+for i in cetesb_stations.code:
+    print('Downloading poll ' + cetesb_stations.name[cetesb_stations.code == i].values + ' Station')
+    qr.all_photo(cetesb_login, cetesb_password,
+                        start_date, end_date, i, csv_photo=True)
+    print('Downloading meteo ' + cetesb_stations.name[cetesb_stations.code == i].values + ' Station')
+    qr.all_met(cetesb_login, cetesb_password,
+                        start_date, end_date, i, csv_met=True)
